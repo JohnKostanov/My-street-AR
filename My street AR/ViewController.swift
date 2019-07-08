4a6ce5f45bbe11dacc7e89b5cd1d60689baf4345
JohnKostanov <kostanovd@icloud.com>
2019-07-08 18:14:16 +0200

    Initial commit

--- My street AR/ViewController.swift
+++ My street AR/ViewController.swift
@@ -6,8 +6,6 @@
 //  Copyright © 2019 John Kostanov. All rights reserved.
 //
 
-import UIKit
-import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
@@ -16,60 +14,76 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let myHouse = createHome()
+        myHouse.position = SCNVector3(-2, -2, -5)
+        sceneView.scene.rootNode.addChildNode(myHouse)
         
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
         // Run the view's session
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
         // Pause the view's session
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
+    func createHome() -> SCNNode {
+       
+        let house = UIImage(named: "art.scnassets/house.jpeg")
+        let materialsColor = [house, house, house, house, house, house]
+        let materialHouse = materialsColor.map { color -> SCNMaterial in
+            let material = SCNMaterial()
+            material.diffuse.contents = color
+            return material
+        }
+        
+        let box = SCNBox(width: 1, height: 3, length: 10, chamferRadius: 0)
+        box.materials = materialHouse
+        let buildingHouse = SCNNode(geometry: box)
+        
+
+        let roadImage = UIImage(named: "art.scnassets/texture_asphalt.jpg")
+        let roadColor = [roadImage]
+        let roadMaterial = roadColor.map { color -> SCNMaterial in
+            let material = SCNMaterial()
+            material.diffuse.contents = color
+            return material
+        }
+        let street = SCNPlane(width: 5, height: 15)
+        street.materials = roadMaterial
+        let streetNode = SCNNode(geometry: street)
+        streetNode.eulerAngles.x = -.pi / 2
+        streetNode.position.y = -2.001
+        buildingHouse.addChildNode(streetNode)
+        
+        for z in stride(from: -4, through: 6, by: 2) {
+            let tree = createTree()
+            tree.position = SCNVector3(2, -1, z)
+            buildingHouse.addChildNode(tree)
+        }
+        return buildingHouse
+    }
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
+    func createTree() -> SCNNode {
+        let stall = SCNCylinder(radius: 0.1, height: 2)
+        stall.firstMaterial?.diffuse.contents = UIColor.brown
+        let node = SCNNode(geometry: stall)
+        
+        let crown = SCNSphere(radius: 0.7)
+        crown.firstMaterial?.diffuse.contents = UIColor.green
+        let crownNode = SCNNode(geometry: crown)
+        crownNode.position.y = 1
+        node.addChildNode(crownNode)
+        node.scale = SCNVector3(0.5, 0.5, 0.5)
+        
         return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
